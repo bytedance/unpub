5a5c59f6af5087c37c0bf07bd144baac5f8f22db
tanzhanwu <tanzhanwu@lizhi.fm>
2021-08-18 09:06:31 +0800

    修复头部显示bug2

--- unpub/example/main.dart
+++ unpub/example/main.dart
@@ -14,6 +14,6 @@ main(List<String> args) async {
     packageStore: packageStore,
   );
 
-  var server = await app.serve('0.0.0.0', 8000);
+  var server = await app.serve('0.0.0.0', 4000);
   print('Serving at http://${server.address.host}:${server.port}');
 }
